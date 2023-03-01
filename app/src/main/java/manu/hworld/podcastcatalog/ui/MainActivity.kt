2de441f4357320328a5091fc34e9b10911ec087d
Manu Rodriguez <manu@gmail.com>
2023-03-01 15:37:51 +0100

    initial commit

---
+++ app/src/main/java/manu/hworld/podcastcatalog/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package manu.hworld.podcastcatalog.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import manu.hworld.podcastcatalog.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
